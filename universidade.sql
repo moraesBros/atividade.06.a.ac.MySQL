edec821807cdc2c8c71eae0f44ab40f50ecdae16
moraesBros <161986858+moraesBros@users.noreply.github.com>
2025-04-27 20:28:32 -0300

    Create universidade.sql

---
+++ universidade.sql
@@ -0,0 +1,119 @@
+-- Banco de dados Universidade.
+CREATE DATABASE Universidade;
+USE Universidade;
+
+-- Tabela Grupo.
+CREATE TABLE Grupo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    grade VARCHAR(255) NOT NULL,
+    semestre INT(2) NOT NULL,
+    ano INT(4) NOT NULL,
+    curso_id INT,
+    seção_id INT,
+    FOREIGN KEY (curso_id) REFERENCES Curso(id),
+    FOREIGN KEY (seção_id) REFERENCES Seção(id),
+);
+
+-- Tabela Instrutor.
+CREATE TABLE Instrutor (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    salario DECIMAL(9,2) NOT NULL,
+    departamento_id INT,
+    FOREIGN KEY (departamento_id) REFERENCES Departamento(id)
+);
+
+-- Tabela Requisitos.
+CREATE TABLE Requisitos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao TEXT NOT NULL,
+    curso_id INT,
+    FOREIGN KEY (curso_id) REFERENCES Curso(id)
+);
+
+-- Tabela Professor.
+CREATE TABLE Professor (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    especialidade VARCHAR(255),
+    salario DECIMAL(9,2) NOT NULL,
+    semestre INT(2) NOT NULL,
+    ano INT(4) NOT NULL,
+    curso_id INT,
+    seção_id INT,
+    FOREIGN KEY (curso_id) REFERENCES Curso(id),
+    FOREIGN KEY (seção_id) REFERENCES Seção(id),
+);
+
+-- Tabela Seção.
+CREATE TABLE Secao (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    semestre INT(2) NOT NULL,
+    ano INT(4) NOT NULL,
+    predio VARCHAR(255) NOT NULL,
+    curso_id INT,
+    instrutor_id INT,
+    horario_id INT,
+    FOREIGN KEY (curso_id) REFERENCES Curso(id),
+    FOREIGN KEY (instrutor_id) REFERENCES Instrutor(id),
+    FOREIGN KEY (horário_id) REFERENCES Horário(id)
+);
+
+-- Tabela Sala de aula.
+CREATE TABLE SalaDeAula (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    predio VARCHAR(255) NOT NULL,
+    numero INT NOT NULL,
+    capacidade INT
+);
+
+-- Tabela Horário.
+CREATE TABLE Horario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dia_semana VARCHAR(255) NOT NULL,
+    horario_inicio TIME NOT NULL,
+    horario_fim TIME NOT NULL,
+    sala_id INT,
+    FOREIGN KEY (sala_id) REFERENCES SalaDeAula(id)
+);
+
+-- Tabela Estudante.
+CREATE TABLE Estudante (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    nota DECIMAL(4,2),
+    curso_id INT,
+    grupo_id INT,
+    departamento_id INT,
+    FOREIGN KEY (curso_id) REFERENCES Curso(id),
+    FOREIGN KEY (grupo_id) REFERENCES Grupo(id),
+    FOREIGN KEY (departamento_id) REFERENCES Departamento(id)
+);
+
+-- Tabela Curso.
+CREATE TABLE Curso (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    nota DECIMAL(4,2),
+    departamento_id INT,
+    FOREIGN KEY (departamento_id) REFERENCES Departamento(id)
+);
+
+-- Tabela Departamento.
+CREATE TABLE Departamento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    predio VARCHAR(255) NOT NULL,
+    orçamento INT(12) NOT NULL
+);
+
+-- Tabela Conselheiro.
+CREATE TABLE Conselheiro (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    instrutor_id INT, 
+    estudante_id INT,
+    FOREIGN KEY (estudante_id) REFERENCES Estudante(id),
+    FOREIGN KEY (instrutor_id) REFERENCES Instrutor(id)
+);
